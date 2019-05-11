b291aea58761150c4b4ef2be0f218adb13aa9778
Eric Palacios <epalace@gmail.com>
2019-05-11 14:10:41 +0200

    A* first implementation

--- src/main/scala/flood/Solver.scala
+++ src/main/scala/flood/Solver.scala
@@ -1,6 +1,7 @@
 package flood
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 object Solver {
 
@@ -18,15 +19,60 @@ object Solver {
           val floodCount = newBoard.floodCount
 
           (newBoard, color, floodCount)
-        }).maxBy { case (_, color, floodCount) =>
-          (floodCount, -color)
-
-        }
+        }).maxBy { case (_, color, floodCount) => (floodCount, -color) }
 
         recGreedy(nextBoard, maxFloodCount, accum :+ bestColor)
       }
     }
     recGreedy(board, board.floodCount, Vector.empty)
+  }
+
+  val ordering:Ordering[(Board, Vector[Int])] = Ordering.by[(Board, Vector[Int]), (Int, Iterable[Int])] {
+    case (board, movements) => (movements.size, movements)
+  }(Ordering.Tuple2(Ordering.Int, Ordering.Iterable[Int])).reverse
+
+//  def ordering = Ordering.fromLessThan[(Board, Vector[Int])] {
+//    case (a, b) =>
+//      a._2.size < b._2.size
+////      if (a._2.size < b._2.size) {
+////        true
+////      } else {
+////        Ordering.Iterable[Int].lteq(a._2, b._2)
+////      }
+//  }.reverse
+
+  def AStar(board: Board): Seq[Int] = {
+    val numColors = board.numColors
+    val closedSet = mutable.Set.empty[Board]
+    val openSet = mutable.PriorityQueue.empty[(Board, Vector[Int])](ordering)
+
+    @tailrec
+    def recAStar: Vector[Int] = {
+      //println(s"QUEUE:\n${openSet.toList.map(_._2).mkString("\n")}\n")
+      val (board, mv) = openSet.dequeue()
+      //println(s"DEQUEUED ${mv}")
+      if (closedSet.contains(board)) {
+        recAStar
+      } else {
+        closedSet += board
+        if (board.floodCount == board.size * board.size) {
+          mv
+        } else {
+          val newNodes = for {
+            color <- 0 until numColors if color != board.matrix(0)(0)
+          } yield {
+            val newBoard = board.flood(color)
+            (newBoard, mv :+ color)
+          }
+
+          openSet ++= newNodes
+          recAStar
+        }
+      }
+    }
+
+    openSet +=((board, Vector.empty))
+    recAStar
   }
 
 }
--- src/test/scala/flood/SolverSpec.scala
+++ src/test/scala/flood/SolverSpec.scala
@@ -61,21 +61,81 @@ class SolverSpec extends Specification {
       Solver.greedy(board) must_==Seq(2, 0, 1, 2)
     }
 
-    "greedy random " >> {
-      val board = Board.random(10, 10)
-      println(board)
-      val movements = Solver.greedy(board)
-      println(movements)
-
-      movements.foldLeft(board) { case (board, nextColor) =>
-        val nextBoard = board.flood(nextColor)
-        println(nextBoard)
-        println()
-        nextBoard
-      }
-
-      true
+    "A* trivial" >> {
+      Solver.greedy(Board.parse("1")) must beEmpty
     }
+
+    "A* trivial (2) " >> {
+      val board = Board.parse(
+        """
+          |1, 1, 1, 1, 1
+          |1, 1, 1, 1, 1
+          |1, 1, 1, 1, 1
+          |1, 1, 1, 1, 1
+          |1, 1, 1, 1, 1
+        """.stripMargin)
+      Solver.AStar(board) must beEmpty
+    }
+
+
+    "A* 1 change " >> {
+      val board = Board.parse(
+        """
+          |1, 1, 1, 1, 1
+          |1, 1, 1, 1, 1
+          |1, 1, 1, 1, 1
+          |1, 1, 0, 0, 1
+          |1, 1, 1, 1, 1
+        """.stripMargin)
+
+      Solver.AStar(board) must_==Seq(0)
+    }
+
+    "A* 2 change " >> {
+      val board = Board.parse(
+        """
+          |1, 1, 1, 1, 1
+          |1, 1, 1, 1, 1
+          |1, 1, 1, 1, 1
+          |1, 1, 2, 0, 1
+          |1, 1, 1, 1, 1
+        """.stripMargin)
+
+      Solver.AStar(board) must_==Seq(0, 2)
+    }
+
+    "A* 3 change " >> {
+      val board = Board.parse(
+        """
+          |1, 1, 1, 0, 1
+          |1, 1, 1, 0, 2
+          |1, 1, 1, 0, 0
+          |1, 1, 2, 2, 2
+          |1, 1, 2, 2, 2
+        """.stripMargin)
+
+      Solver.AStar(board) must_==Seq(0, 1, 2)
+    }
+
+
+//    "greedy random " >> {
+//      val board = Board.random(15, 4)
+//
+//      println(board)
+//      val greedy = Solver.greedy(board)
+//      val astar = Solver.AStar(board)
+//      println(s"Greedy: ${greedy.size} ${greedy}")
+//      println(s"AStar: ${astar.size} ${astar}")
+//
+////      movements.foldLeft(board) { case (board, nextColor) =>
+////        val nextBoard = board.flood(nextColor)
+////        println(nextBoard)
+////        println()
+////        nextBoard
+////      }
+//
+//      true
+//    }
 
   }
 }
